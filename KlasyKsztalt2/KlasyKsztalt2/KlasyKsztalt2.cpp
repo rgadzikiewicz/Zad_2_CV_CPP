4c71e517b4e14d7f3c7eb438e3243fd5fff2e84a
rgadzikiewicz <r.gadzikiewicz@wp.pl>
2016-01-25 22:44:11 +0100

    Zad_2_CV_CPP
    zadanie 2

---
+++ KlasyKsztalt2/KlasyKsztalt2/KlasyKsztalt2.cpp
@@ -0,0 +1,125 @@
+// KlasyKsztalt.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+class Ksztalt1
+{
+public:
+	virtual ~Ksztalt1()
+	{ }
+
+	virtual void rysuj1() = 0; // metoda czysto wirtualna
+};
+
+class Ksztalt2
+{
+public:
+	virtual ~Ksztalt2()
+	{ }
+
+	virtual void rysuj2() // metoda wirtualna
+	{
+		std::cout << "Zostanie przesloniete przez prostokat i trojkat" << std::endl;
+	}
+};
+
+class Kolo
+	: public Ksztalt1
+{
+public:
+	virtual void rysuj1()
+	{
+		std::cout << "Rysuje kolo" << std::endl;
+	}
+};
+
+class Kwadrat
+	: public Ksztalt1
+{
+public:
+	virtual void rysuj1()
+	{
+		std::cout << "Rysuje kwadrat" << std::endl;
+	}
+};
+
+class Prostokat
+	: public Ksztalt2
+{
+public:
+	virtual void rysuj2()
+	{
+		std::cout << "Rysuje prostokat" << std::endl;
+	}
+};
+
+class Trojkat
+	: public Ksztalt2
+{
+public:
+	virtual void rysuj2()
+	{
+		std::cout << "Rysuje trojkat" << std::endl;
+	}
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Ksztalt1 *a = new Kolo, *b = new Kwadrat;
+	Ksztalt2 *c = new Prostokat, *d = new Trojkat;
+
+	//----------------- arytmetyka wskaznikow ------------------------------
+	int q = 1;
+	int *wsk;
+	wsk = &q;
+	*wsk += 1;
+	std::cout << "\nTeraz wsk = " << *wsk << std::endl;
+	//----------------------------------------------------------------------
+
+	for (int i = 0; i < 4; i++)
+	{
+		a->rysuj1();
+	}
+
+	int k = 0;
+	while (k < 3)
+	{
+		b->rysuj1();
+		k++;
+	}
+
+	int j = 0;
+	do
+	{
+		c->rysuj2();
+		j++;
+	} while (j < 2);
+
+	d->rysuj2();
+
+	//char ch = 's';
+	//std::string str(5, ch);
+	//std::cout << str;
+
+	//std::string str("m");
+	//char ch = str[0];
+	//std::cout << ch;
+
+	//----------------------------- konwersja typow -----------------------------
+	std::string str("witaj");
+	std::cout << str.c_str() << std::endl;
+	//---------------------------------------------------------------------------
+
+	delete a;
+	delete b;
+	delete c;
+	delete d;
+
+	system("PAUSE");
+	return 0;
+}
+
+
